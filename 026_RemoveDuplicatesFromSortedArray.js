574e65583219a46bacfbeab2a212197876a374d5
Samuell <samuellmarckson30@gmail.com>
2024-01-07 11:23:23 -0300

    Remove Duplicates From Sorted Array

---
+++ 026_RemoveDuplicatesFromSortedArray.js
@@ -0,0 +1,13 @@
+// Source: https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+var removeDuplicates = function (nums) {
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+};
+
+console.log(removeDuplicates([1, 1, 2]));
