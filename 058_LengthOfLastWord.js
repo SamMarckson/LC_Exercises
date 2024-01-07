814203e75eef9f00405171564c0ea8e966e3693e
Samuell <samuellmarckson30@gmail.com>
2024-01-07 10:04:57 -0300

    Length of Last Word - Second Solution

--- 058_LengthOfLastWord.js
+++ 058_LengthOfLastWord.js
@@ -1,6 +1,19 @@
 // Source: https://leetcode.com/problems/length-of-last-word/description/
 
+// First Solution
 var lengthOfLastWord = function (s) {
   s = s.trim().split(" ");
   return s[s.length - 1].length;
 };
+
+// Second Solution
+// var lengthOfLastWord = function (s) {
+//   s = s.trim();
+//   let counter = 0;
+//   for (let i = s.length - 1; i >= 0; i--) {
+//     if (s[i] === " ") break;
+//     counter += 1;
+//   }
+
+//   return counter;
+// };
