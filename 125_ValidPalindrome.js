2125c0f6c313bb613dcfa4be68e24ec7a10a0bf1
Samuell <samuellmarckson30@gmail.com>
2024-01-14 12:58:44 -0300

    Valid Palindrome

---
+++ 125_ValidPalindrome.js
@@ -0,0 +1,17 @@
+// Source: https://leetcode.com/problems/valid-palindrome/description/
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    for(let i = 0; i < Math.floor(s.length / 2); i++) {
+      if(s[i] !== s[s.length - (i + 1)]) {
+        return false
+      }
+    }
+    return true;
+};
+
+
+console.log(isPalindrome(" "));
