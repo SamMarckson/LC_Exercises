3c487ad6f7b0c5038be4f21e32793d88ceff10d1
Samuell <samuellmarckson30@gmail.com>
2024-01-14 13:28:17 -0300

    Valid Palindrome in PHP

---
+++ 125_ValidPalindrome.php
@@ -0,0 +1,24 @@
+<?php
+
+class Solution {
+
+  /**
+   * @param String $s
+   * @return Boolean
+   */
+  function isPalindrome($s) {
+    $s = preg_replace("/[^a-zA-Z0-9]/", "", strtolower($s));
+    echo $s;
+    for ($i = 0; $i < floor(strlen($s) / 2); $i++) {
+      if ($s[$i] !== $s[strlen($s) - ($i + 1)]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+$response = new Solution();
+echo $response->isPalindrome("A man, a plan, a canal: Panama");
+
+?>
