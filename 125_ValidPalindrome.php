ea4e9ae5ed05a75217470a87b9d8b7ee33488ecf
Samuell <samuellmarckson30@gmail.com>
2024-01-20 10:37:47 -0300

    Valid Palindorme

--- 125_ValidPalindrome.php
+++ 125_ValidPalindrome.php
@@ -8,7 +8,6 @@ class Solution {
    */
   function isPalindrome($s) {
     $s = preg_replace("/[^a-zA-Z0-9]/", "", strtolower($s));
-    echo $s;
     for ($i = 0; $i < floor(strlen($s) / 2); $i++) {
       if ($s[$i] !== $s[strlen($s) - ($i + 1)]) {
         return false;
