0366086fe0466bd07cac9eeb7513502302ae85b1
Samuell <samuellmarckson30@gmail.com>
2024-04-08 19:13:27 -0300

    Reverse String LC

---
+++ 344_ReverseString.js
@@ -0,0 +1,10 @@
+// SOURCE: https://leetcode.com/problems/reverse-string/description/
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+
+// FIRTS SOLUTION
+var reverseString = function (s) {
+  return s.reverse();
+};
