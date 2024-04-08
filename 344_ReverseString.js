35c9fb8c4ad309adac0d74b79f3f7dcd430d26a1
Samuell <samuellmarckson30@gmail.com>
2024-04-08 19:14:30 -0300

    Reverse String LC | Second Solution

--- 344_ReverseString.js
+++ 344_ReverseString.js
@@ -4,7 +4,18 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 
-// FIRTS SOLUTION
+// SOLUTION ONE
 var reverseString = function (s) {
-  return s.reverse();
+  let helper;
+  for (let i = 0; i < s.length / 2; i++) {
+    helper = s[i];
+    s[i] = s[s.length - (i + 1)];
+    s[s.length - (i + 1)] = helper;
+  }
+  return s;
 };
+
+// FIRTS SOLUTION
+// var reverseString = function (s) {
+//   return s.reverse();
+// };
