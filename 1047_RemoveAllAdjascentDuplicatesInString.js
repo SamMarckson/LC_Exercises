c2abf04379651e4e2f6596786420216ce394225c
Samuell <samuellmarckson30@gmail.com>
2024-04-27 18:30:01 -0300

    Remove All Adjascent Duplicates In String - LeetCode

---
+++ 1047_RemoveAllAdjascentDuplicatesInString.js
@@ -0,0 +1,17 @@
+// Source: https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/description/
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function (s) {
+  const stack = [s[0]];
+
+  for (let i = 1; i < s.length; i++) {
+    if (s[i] !== stack[stack.length - 1]) {
+      stack.push(s[i]);
+    } else {
+      stack.pop();
+    }
+  }
+  return stack.join("");
+};
