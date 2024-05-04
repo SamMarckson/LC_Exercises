299117fef38130f07f5664aa4a80896cc7c89518
Samuell <samuellmarckson30@gmail.com>
2024-05-04 13:39:23 -0300

    Quick Sort Algorithm

--- QuickSort.js
+++ QuickSort.js
@@ -1,14 +1,7 @@
-// Source: https://leetcode.com/problems/sort-colors/description/
-
 // Making use of Quick Sort Algorithm
 
-/**
- * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
- */
-
 var sortColors = function (nums) {
-  if(nums.length < 2) {
+  if (nums.length < 2) {
     return nums;
   }
 
@@ -16,14 +9,14 @@ var sortColors = function (nums) {
   const leftArr = [];
   const rightArr = [];
 
-  for(let i = 0; i < nums.length - 1; i++) {
-    if(nums[i] < pivotElement) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] < pivotElement) {
       leftArr.push(nums[i]);
     } else {
       rightArr.push(nums[i]);
     }
   }
-  return[...sortColors(leftArr), pivotElement, ...sortColors(rightArr)]
+  return [...sortColors(leftArr), pivotElement, ...sortColors(rightArr)];
 };
 
-console.log(sortColors([2,0,2,1,1,0]));
+console.log(sortColors([2, 0, 2, 1, 1, 0]));
